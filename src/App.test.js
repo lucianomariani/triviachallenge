6b45d9d317008e1045e71390afac2d5ce430e14f
luchomariani <lucho.mariani@gmail.com>
2019-11-27 16:08:15 -0300

    circleci test 6

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
+const wrapper = shallow(<App />) 
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapper.find(App))
 });
