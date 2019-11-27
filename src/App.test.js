1fcdaabd0f793ae9705e4889ffe8a51997d4dfd8
luchomariani <lucho.mariani@gmail.com>
2019-11-27 16:39:23 -0300

    circleci test 10

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App.js';
+import Home from './containers/Home';
+import Game from "./containers/Game";
+import Results from "./containers/Results";
 
 const wrapper = shallow(<App />) 
 
